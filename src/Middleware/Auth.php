10e040b0f9ecc8b8b766c28b2b8bed3c4ddc47f0
kevnpessoa <kevnpessoa@gmail.com>
2020-06-15 21:11:37 -0400

    auth

--- src/Middleware/Auth.php
+++ src/Middleware/Auth.php
@@ -4,5 +4,13 @@ namespace Napps\Rest\Middleware;
 
 class Auth
 {
-    //
+    public function login() : bool
+    {
+        //
+    }
+
+    public function logout() : bool
+    {
+        //
+    }
 }
